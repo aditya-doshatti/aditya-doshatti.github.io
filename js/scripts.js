6bbb9702acbc7efc080eab13c98f9788e3b3d7bd
Aditya Doshatti <aditya.doshatti@sjsu.edu>
2018-12-27 18:32:42 -0800

    Changes to add text on the other page and some animation

--- js/scripts.js
+++ js/scripts.js
@@ -30,20 +30,4 @@
             documElem.scrollTop(0);
         }
     });
-})();      
-$window.on('scroll', revealOnScroll);
-(function revealOnScroll() {
-    var scrolled = $window.scrollTop(), win_height_padded = $window.height() * 1.1;
-    $('.revealOnScroll:not(.animated)').each(function(){
-        var $this = $(this), offsetTop = $this.offset().top;
-        if (scrolled + win_height_padded > offsetTop) {
-            $this.addClass('animated ' + $this.data('animation'))
-        }
-    });
-    $('.revealOnScroll.animated').each(function (index){
-        var $this = $(this), offsetTop = $this.offset().top;
-        if (scrolled + win_height_padded > offsetTop) {
-            $(this).removeClass('animated fadeInUp flipInX lightSpeedIn')
-        }
-    });
-})();
+})(); 
